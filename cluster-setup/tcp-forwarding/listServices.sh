27beb39bf8afd34c9d9d7adf4f427078d0095e93
Kunal Gupta <assaxor@gmail.com>
2023-03-28 20:35:58 +0000

    fix(k3d): update timezone to use "Calcutta"

--- cluster-setup/tcp-forwarding/listServices.sh
+++ cluster-setup/tcp-forwarding/listServices.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
-kubectl --context kind-"$(yq -r '.name' "$(dirname "$0")/../kind/cluster-config.yaml" )" get services --all-namespaces -o json | jq -r '.items[] | { name: .metadata.name, namespace: .metadata.namespace, ports: .spec.ports, ip: .status.loadBalancer?|.ingress[]?|.ip  }' | jq -s
+#cluster_name=kind-"$(yq -r '.name' "$(dirname "$0")/../kind/cluster-config.yaml" )"
+cluster_name=k3d-"$(yq -r '.metadata.name' "$(dirname "$0")/../k3d/cluster-config.yaml" )"
+
+kubectl --context "$cluster_name" get services --all-namespaces -o json | jq -r '.items[] | { name: .metadata.name, namespace: .metadata.namespace, ports: .spec.ports, ip: .status.loadBalancer?|.ingress[]?|.ip  }' | jq -s
